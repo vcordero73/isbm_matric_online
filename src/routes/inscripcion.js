d2b03ff5d11cdfe0c4a2e5150b5b0a9df29e5a73
victor orlando cordero <victorcordero73@gmail.com>
2020-11-02 19:11:25 -0300

    segundo commit

--- src/routes/inscripcion.js
+++ src/routes/inscripcion.js
@@ -143,7 +143,7 @@ router.post('/',[
                     res.redirect('/inscripcion');
                    }
                 }
-                
+                                 
 
                   
               }
